5a9c309fd3b9398ecb7442614187f65a61fa4ea4
Bryant <bchhun@gmail.com>
2020-03-07 13:27:06 -0800

    towards proper segmentation

--- pipeline/segmentation_validation.py
+++ pipeline/segmentation_validation.py
@@ -7,130 +7,152 @@ import skimage.io as io
 
 
 def find_rim(cell_positions):
-  masks = set(tuple(r) for r in cell_positions)
-  inner_masks = set((r[0]-1, r[1]) for r in masks) & \
+    masks = set(tuple(r) for r in cell_positions)
+    inner_masks = set((r[0]-1, r[1]) for r in masks) & \
                 set((r[0]+1, r[1]) for r in masks) & \
                 set((r[0], r[1]-1) for r in masks) & \
                 set((r[0], r[1]+1) for r in masks)
-  edge_positions = np.array(list(masks - inner_masks))
-  return edge_positions
+    edge_positions = np.array(list(masks - inner_masks))
+    return edge_positions
 
 
 def drawContour(m, s, c, RGB):
-  """Draw edges of contour 'c' from segmented image 's' onto 'm' in colour 'RGB'"""
-  # Fill contour "c" with white, make all else black
-  #     thisContour = s.point(lambda p:p==c and 255)
-  # DEBUG: thisContour.save(f"interim{c}.png")
-  #     thisContour = s.point(lambda p:p==c and 255)
-  thisContour = s.point(lambda x: 255 if x > 30 else False)
+    """Draw edges of contour 'c' from segmented image 's' onto 'm' in colour 'RGB'"""
+    # Fill contour "c" with white, make all else black
+    #     thisContour = s.point(lambda p:p==c and 255)
+    # DEBUG: thisContour.save(f"interim{c}.png")
+    #     thisContour = s.point(lambda p:p==c and 255)
+    thisContour = s.point(lambda x: 255 if x > 30 else False)
 
-  # Find edges of this contour and make into Numpy array
-  thisEdges = thisContour.filter(ImageFilter.FIND_EDGES)
-  thisEdgesN = np.array(thisEdges)
+    # Find edges of this contour and make into Numpy array
+    thisEdges = thisContour.filter(ImageFilter.FIND_EDGES)
+    thisEdgesN = np.array(thisEdges)
 
-  # Paint locations of found edges in color "RGB" onto "main"
-  m[np.nonzero(thisEdgesN)] = RGB
-  return m
+    # Paint locations of found edges in color "RGB" onto "main"
+    m[np.nonzero(thisEdgesN)] = RGB
+    return m
 
 
 def rescale_plot(arr1, filename, size=(1108, 1108), dpi=500):
-  plt.clf()
+    plt.clf()
 
-  if type(arr1) == np.ndarray:
+    if type(arr1) == np.ndarray:
     #         arr1 = auto_contrast_slice(arr1)
-    im = Image.fromarray(arr1)
-  else:
-    im = arr1
-  im = im.resize(size)
+        im = Image.fromarray(arr1)
+    else:
+        im = arr1
+    im = im.resize(size)
 
-  ax = plt.axes([0, 0, 1, 1], frameon=False)
-  ax.get_xaxis().set_visible(False)
-  ax.get_yaxis().set_visible(False)
-  plt.autoscale(tight=True)
-  plt.imshow(np.array(im))
-  if type(arr1) == np.ndarray:
-    plt.clim(0, 0.9 * arr1.max())
-  plt.savefig(filename, bbox_inches='tight', pad_inches=0, dpi=dpi)
+    ax = plt.axes([0, 0, 1, 1], frameon=False)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.autoscale(tight=True)
+    plt.imshow(np.array(im))
+    if type(arr1) == np.ndarray:
+        plt.clim(0, 0.9 * arr1.max())
+    plt.savefig(filename, bbox_inches='tight', pad_inches=0, dpi=dpi)
 
 
 # %%
 def load_and_plot(img_rgb, img_grey, output):
-  phase = Image.open(img_rgb).convert('L').convert('RGB')
-  segment = Image.open(img_grey).convert('L')
+    phase = Image.open(img_rgb).convert('L').convert('RGB')
+    segment = Image.open(img_grey).convert('L')
 
-  phaseN = np.array(phase)
-  phaseN = drawContour(phaseN, segment, 0, (255, 0, 0))
-  Image.fromarray(phaseN).save(output)
+    phaseN = np.array(phase)
+    phaseN = drawContour(phaseN, segment, 0, (255, 0, 0))
+    Image.fromarray(phaseN).save(output)
 
 
-def segmentation_validation_michael(paths, id):
+def segmentation_validation_michael(paths, category, gpu_id):
+    """
 
-  temp_folder, supp_folder, target, sites = paths[0], paths[1], paths[2], paths[3]
+    :param paths:
+    :param category: str
+        only "mg", "nonmg", "both", "unfiltered"
+    :param gpu_id:
+    :return:
+    """
 
-  if "NOVEMBER" in temp_folder:
-      date = "NOVEMBER"
-  elif "JANUARY" in temp_folder:
-      date = "JANUARY"
-  else:
-      date = "JAN_FAST"
+    temp_folder, supp_folder, target, sites = paths[0], paths[1], paths[2], paths[3]
 
-  for site in sites:
-    print(f"building full frame validation for {site} from {temp_folder}")
+    if "NOVEMBER" in temp_folder:
+        date = "NOVEMBER"
+    elif "JANUARY" in temp_folder:
+        date = "JANUARY"
+    else:
+        date = "JAN_FAST"
 
-    stack_path = os.path.join(temp_folder + '/' + site + '.npy')
-    raw_input_stack = np.load(stack_path)
+    for site in sites:
+        print(f"building full frame validation for {site} from {temp_folder}")
 
-    NN_predictions_stack = np.load(temp_folder + '/%s_NNProbabilities.npy' % site)
-    cell_pixels = pickle.load(open(supp_folder + f"/{site[0:2]}-supps/{site}/cell_pixel_assignments.pkl", 'rb'))
+        stack_path = os.path.join(temp_folder + '/' + site + '.npy')
+        raw_input_stack = np.load(stack_path)
 
-    # Results of count thresholding are NOT used in the generation of these assignments
-    # the thresholding values are used to filter the "cell_positions.pkl" =
-    #   (mg_cell_positions, non_mg_cell_positions, other_cells)
-    #   each of mg_cell_positions, non_mg_cell_positions ... is of format (cell_id, mean_pos)
-    #   cell_pixels is of shape=(t_points, 2) :: cell_pixels[t_point] = (positions, inds)
-    #   cell_id is a subset of "inds"
+        NN_predictions_stack = np.load(temp_folder + '/%s_NNProbabilities.npy' % site)
+        cell_pixels = pickle.load(open(supp_folder + f"/{site[0:2]}-supps/{site}/cell_pixel_assignments.pkl", 'rb'))
 
-    # my attempt to include these
-    filtered_positions = pickle.load(
-        open(supp_folder + f"/{site[0:2]}-supps/{site}/cell_positions.pkl", 'rb'))
+        # option to include filtered positions
+        filtered_positions = pickle.load(
+            open(supp_folder + f"/{site[0:2]}-supps/{site}/cell_positions.pkl", 'rb'))
 
-    stack = []
-    for t_point in range(len(raw_input_stack)):
-      mat = raw_input_stack[t_point, :, :, 0]
-      mat = np.stack([mat] * 3, 2)
-      (mg_cell_positions, non_mg_cell_positions, other_cells) = filtered_positions[t_point]
+        stack = []
+        for t_point in range(len(raw_input_stack)):
+            mat = raw_input_stack[t_point, :, :, 0]
+            mat = np.stack([mat] * 3, 2)
 
-      # "inds" should be the same as "position_labels" in instance_clustering
-      positions, inds = cell_pixels[t_point]
-      for cell_ind in np.unique(inds):
-        if cell_ind < 0:
-          continue
+            # this block represents rendering of FILTERED MG and nonMG cells
+            (mg_cell_positions, non_mg_cell_positions, other_cells) = filtered_positions[t_point]
 
-        ForkedPdb().set_trace()
-        cell_positions = positions[np.where(inds == cell_ind)]
+            # this block represents rendering of MG and nonMG cells, but NOT filtered by size.
+            positions, inds = cell_pixels[t_point]
 
-        # filter by MG and non MG
-        print('including mg and nonmg that pass count thresholding')
-        cell_positions = cell_positions[np.where(inds == mg_cell_positions[0])]
-        cell_positions = cell_positions[np.where(inds == non_mg_cell_positions[0])]
+            if 'unfiltered' in category:
+                for cell_ind in np.unique(inds):
+                    new_mat = _append_segmentation(positions, inds, cell_ind, NN_predictions_stack, t_point, mat)
+                    if new_mat:
+                        stack.append(new_mat)
+            elif 'both' in category:
+                for both_cell_id, _ in np.unique(mg_cell_positions.extend(non_mg_cell_positions)):
+                    new_mat = _append_segmentation(positions, inds, both_cell_id, NN_predictions_stack, t_point, mat)
+                    if new_mat:
+                        stack.append(new_mat)
+            elif 'mg' in category:
+                for mg_cell_id, _ in np.unique(mg_cell_positions):
+                    new_mat = _append_segmentation(positions, inds, mg_cell_id, NN_predictions_stack, t_point, mat)
+                    if new_mat:
+                        stack.append(new_mat)
+            elif 'nonmg' in category:
+                for non_mg_cell_id, _ in np.unique(non_mg_cell_positions):
+                    new_mat = _append_segmentation(positions, inds, non_mg_cell_id, NN_predictions_stack, t_point, mat)
+                    if new_mat:
+                        stack.append(new_mat)
+            else:
+                raise NotImplementedError(f"rendering category of type {category} is not impemented")
 
-        outer_rim = find_rim(cell_positions)
-        mask_identities = NN_predictions_stack[t_point][(cell_positions[:, 0], cell_positions[:, 1])].mean(0)
-        if mask_identities[1] > mask_identities[2]:
-          c = 'b'
-          mat[(outer_rim[:, 0], outer_rim[:, 1])] = np.array([0, 65535, 0]).reshape((1, 3))
-        else:
-          c = 'r'
-          mat[(outer_rim[:, 0], outer_rim[:, 1])] = np.array([65535, 0, 0]).reshape((1, 3))
-      stack.append(mat)
+        # tifffile.imwrite(target+'/'+f'{date}_{site}_predictions.tiff', np.stack(stack, 0))
+        # np.save(target+'/'+f'{date}_{site}_predictions.npy', np.stack(stack, 0))
 
-    # tifffile.imwrite(target+'/'+f'{date}_{site}_predictions.tiff', np.stack(stack, 0))
-    # np.save(target+'/'+f'{date}_{site}_predictions.npy', np.stack(stack, 0))
+        # using skimage.io to access tifffile on IBM machines
+        io.imsave(target+'/'+f'{date}_{site}_{gpu_id}_predictions.tif',
+                  np.stack(stack, 0).astype("uint16"),
+                  plugin='tifffile')
 
-    # using skimage.io to access tifffile on IBM machines
-    io.imsave(target+'/'+f'{date}_{site}_{id}_predictions.tif',
-              np.stack(stack, 0).astype("uint16"),
-              plugin='tifffile')
+
+def _append_segmentation(positions_, inds_, cell_id_, NN_predictions_stack_, t_point_, output_mat_):
+    if cell_id_ < 0:
+        return None
+
+    cell_positions = positions_[np.where(inds_ == cell_id_)]
+
+    outer_rim = find_rim(cell_positions)
+    mask_identities = NN_predictions_stack_[t_point_][(cell_positions[:, 0], cell_positions[:, 1])].mean(0)
+    if mask_identities[1] > mask_identities[2]:
+        c = 'b'
+        output_mat_[(outer_rim[:, 0], outer_rim[:, 1])] = np.array([0, 65535, 0]).reshape((1, 3))
+    else:
+        c = 'r'
+        output_mat_[(outer_rim[:, 0], outer_rim[:, 1])] = np.array([65535, 0, 0]).reshape((1, 3))
+    return output_mat_
 
 
 def segmentation_validation_bryant(paths, id):
