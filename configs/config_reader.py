701e6f173949135292baae81c7eda1f6f9510c88
smguo <symguo@gmail.com>
2021-03-18 19:56:14 -0700

    Merge branch 'master' into 5d_fix

--- configs/config_reader.py
+++ configs/config_reader.py
@@ -2,12 +2,30 @@ import yaml
 import logging
 
 
-def log_warning(msg, *args, **kwargs):
-    """Log message with level WARNING."""
-    # import logging
+# def log_warning(msg, *args, **kwargs):
+#     """Log message with level WARNING."""
+#     # import logging
+#
+#     logging.getLogger(__name__).warning(msg, *args, **kwargs)
 
-    logging.getLogger(__name__).warning(msg, *args, **kwargs)
 
+# replicate from aicsimageio logging mechanism
+###############################################################################
+
+# modify the logging.ERROR level lower for more info
+# CRITICAL
+# ERROR
+# WARNING
+# INFO
+# DEBUG
+# NOTSET
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)4s: %(module)s:%(lineno)4s %(asctime)s] %(message)s",
+)
+log = logging.getLogger(__name__)
+
+###############################################################################
 
 # to add a new configuration parameter, simply add the string to the appropriate set here
 # FILES = {
@@ -23,6 +41,7 @@ PREPROCESS = {
     'target_dirs',
     'channels',
     'fov',
+    'pos_dir',
     'multipage',
     'z_slice',
     'pos_dir'
@@ -95,7 +114,7 @@ class YamlReader(Object):
         self.config = None
 
         # easy way to assign attributes to each category
-        self.files = Object()
+        # self.files = Object()
         self.preprocess = Object()
         self.patch = Object()
         self.inference = Object()
@@ -105,46 +124,46 @@ class YamlReader(Object):
         with open(yml_config, 'r') as f:
             self.config = yaml.load(f)
 
-            self._parse_files()
+            # self._parse_files()
             self._parse_preprocessing()
             self._parse_patch()
             self._parse_inference()
             self._parse_training()
 
-    def _parse_files(self):
-        for key, value in self.config['files'].items():
-            if key in FILES:
-                setattr(self.files, key, value)
-            else:
-                log_warning(f"yaml FILE config field {key} is not recognized")
+    # def _parse_files(self):
+    #     for key, value in self.config['files'].items():
+    #         if key in FILES:
+    #             setattr(self.files, key, value)
+    #         else:
+    #             log_warning(f"yaml FILE config field {key} is not recognized")
 
     def _parse_preprocessing(self):
         for key, value in self.config['preprocess'].items():
             if key in PREPROCESS:
                 setattr(self.preprocess, key, value)
             else:
-                log_warning(f"yaml PREPROCESS config field {key} is not recognized")
+                log.warning(f"yaml PREPROCESS config field {key} is not recognized")
 
     def _parse_patch(self):
         for key, value in self.config['patch'].items():
             if key in PATCH:
                 setattr(self.patch, key, value)
             else:
-                log_warning(f"yaml PATCH config field {key} is not recognized")
+                log.warning(f"yaml PATCH config field {key} is not recognized")
 
     def _parse_inference(self):
         for key, value in self.config['inference'].items():
             if key in INFERENCE:
                 setattr(self.inference, key, value)
             else:
-                log_warning(f"yaml INFERENCE config field {key} is not recognized")
+                log.warning(f"yaml INFERENCE config field {key} is not recognized")
 
     def _parse_training(self):
         for key, value in self.config['training'].items():
             if key in TRAINING:
                 setattr(self.training, key, value)
             else:
-                log_warning(f"yaml TRAINING config field {key} is not recognized")
+                log.warning(f"yaml TRAINING config field {key} is not recognized")
 
 
 
--- run_preproc.py
+++ run_preproc.py
@@ -1,4 +1,3 @@
-# bchhun, {2020-02-21}
 
 # 1. check input: (n_frames * 2048 * 2048 * 2) channel 0 - phase, channel 1 - retardance
 # 2. adjust channel range
@@ -9,23 +8,38 @@
 from pipeline.preprocess import write_raw_to_npy
 import os
 import fnmatch
+import re
 
 import argparse
 from configs.config_reader import YamlReader
+import logging
+log = logging.getLogger(__name__)
 
 
 def main(input_, output_, config_):
+    """
+    Using supplied config file parameters, prepare specified datasets for downstream analysis
+
+    :param input_: str
+        Path to a single experiment
+    :param output_: str
+        Path to output directory for prepared datasets
+    :param config_: YamlReader
+        YamlReader object containing parsed configuration values
+    :return:
+    """
 
     chans = config_.preprocess.channels
     multi = config_.preprocess.multipage
     z_slice = config_.preprocess.z_slice
     fovs = config_.preprocess.fov
 
-    # build list of all sites we wish to process
+    # === build list or dict of all sites we wish to process ===
 
     # positions are identified by subfolder names
     if config_.preprocess.pos_dir:
-        if fovs is 'all':
+        log.info("pos dir, identifying all subfolders")
+        if fovs == 'all':
             sites = [site for site in os.listdir(input_) if os.path.isdir(os.path.join(input_, site))]
         elif type(fovs) is list:
             sites = [site for site in os.listdir(input_) if os.path.isdir(os.path.join(input_, site)) and site in fovs]
@@ -35,43 +49,45 @@ def main(input_, output_, config_):
     # positions are identified by indicies
     # assume files have name structure "t###_p###_z###"
     elif not config_.preprocess.pos_dir:
+        log.info("no pos dir, identifiying all files")
         sites = {}
         all_files = [f for f in os.listdir(input_)
                      if os.path.isfile(os.path.join(input_, f)) and '_p' in f and '.tif' in f]
 
-        if fovs is 'all':
+        if fovs == 'all':
+            log.info("fovs = all, looping ")
             # for every position index in the file, assign the image to a dict key
             while all_files:
                 pos = [int(p_idx.strip('p')) for p_idx in all_files[0].split('_') if 'p' in p_idx][0]
                 if not pos:
                     all_files.pop(0)
                 if pos in sites.keys():
-                    sites[pos].append(all_files.pop(0))
+                    sites[pos].append(os.path.join(input_, all_files.pop(0)))
                 else:
-                    sites[pos] = [all_files.pop(0)]
+                    sites[pos] = [os.path.join(input_, all_files.pop(0))]
 
         elif type(fovs) is list:
             for fov in fovs:
-                sites[fov] = fnmatch.filter(all_files, f'*p{fov:03d}*')
+                sites[fov] = [os.path.join(input_, f) for f in sorted(fnmatch.filter(all_files, f'*p{fov:03d}*'))]
         else:
             raise NotImplementedError("FOV index expected, or preprocess FOVs must be 'all' or list of positions")
     else:
         raise NotImplementedError("pos_dir must be boolean True/False")
 
     # write sites
-    for site in sites:
+    for site in sorted(sites):
         if not os.path.exists(output_):
             os.makedirs(output_)
 
-        # site represents a folder path
+        # site represents a position folder
         if type(site) is str:
-            s_list = [os.path.join(input_, f) for f in sorted(os.listdir(site))]
+            s_list = [os.path.join(input_, site, f) for f in sorted(os.listdir(os.path.join(input_, site)))]
 
-        # site represents a list of files filtered by position index
-        elif type(site) is list:
-            s_list = site
+        # site represents a position index
+        elif type(site) is int:
+            s_list = sites[site]
         else:
-            print(f"no files found for {site}")
+            log.warning(f"no files found for position = {site}")
             continue
 
         write_raw_to_npy(site, s_list, output_, chans, z_slice, multipage=multi)
@@ -84,19 +100,6 @@ def parse_args():
     :return: namespace containing the arguments passed.
     """
     parser = argparse.ArgumentParser()
-
-    # parser.add_argument(
-    #     '-i', '--input',
-    #     type=str,
-    #     required=False,
-    #     help="Path to multipage-tiff file of format [t, x, y], or to single-page-tiffs",
-    # )
-    # parser.add_argument(
-    #     '-o', '--output',
-    #     type=str,
-    #     required=False,
-    #     help="Path to write results",
-    # )
     parser.add_argument(
         '-c', '--config',
         type=str,
@@ -108,13 +111,10 @@ def parse_args():
 
 
 if __name__ == '__main__':
-    # print(time.asctime(time.localtime(time.time())), flush=True)
     arguments = parse_args()
     config = YamlReader()
     config.read_config(arguments.config)
 
     for (src, target) in list(zip(config.preprocess.image_dirs, config.preprocess.target_dirs)):
         main(src, target, config)
-
-    # print(time.asctime(time.localtime(time.time())), flush=True)
 
